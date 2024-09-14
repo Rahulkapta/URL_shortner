2fa3cf5564a64c19b2979771704506af1ae3297b
Rahulkapta <rahulkumar6006464954@gmail.com>
2024-09-14 14:07:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const {connectToMongoDB} = require("./connect")
+const path = require("path")
+const urlRoute= require("./routes/url")
+const URL = require("./models/url");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
+.then(()=>console.log("MongoDB connected"))
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
+app.use(express.json())
+
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render("Home", {
+        urls: allUrls,
+    });
+})
+
+app.use("/url", urlRoute)
+
+app.get("/url/:shortId", async(req ,res)=>{
+    const shortId= req.params.shortId;
+    const entry=  await URL.findOneAndUpdate({
+        shortId
+    },{ $push: {visitHistory : {timestamp: Date.now()}}})
+
+    res.redirect(entry.redirectURL)
+
+})
+app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`))
